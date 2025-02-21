97c0ec17b8be6693fd2355b6526d73a2555bc329
saraKemily <sarakemili@hotmail.com>
2025-02-21 09:36:39 -0300

    feat(Task): changed buttons to icons

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,16 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskInterFace } from '../../resources/tasks/task.model';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { TasksService } from '../tasks.service';
+import { TruncateTextPipe } from '../../pipes/truncate-text.pipe';
 
 
 @Component({
   selector: 'app-task',
-  imports: [DatePipe],
+  imports: [DatePipe, TruncateTextPipe, CommonModule],
   templateUrl: './task.component.html',
 })
 export class TaskComponent {
   @Input({required: true}) public task!: TaskInterFace;
 
+  @Output() public onViewTask: EventEmitter<TaskInterFace> = new EventEmitter();
+  @Output() public onCompleteTask: EventEmitter<TaskInterFace> = new EventEmitter();
+
   public constructor(public tasksService: TasksService) {}
+
+  public completeTask(): void {
+    this.tasksService.deleteTask(this.task.id!);
+    this.onCompleteTask.emit();
+  }
 }
